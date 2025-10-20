5169e37e8112903641edaf3a492074140ec4bbf2
DivyanshTech06 <divyanshtech06@gmail.com>
2025-10-20 13:38:19 +0530

    feat: create pythUtils.ts

---
+++ packages/dca-backend/src/lib/agenda/jobs/executeDCASwap/utils/pythUtils.ts
@@ -0,0 +1,31 @@
+import { HermesClient } from '@pythnetwork/hermes-client';
+import PythAbi from '@pythnetwork/pyth-sdk-solidity/abis/IPyth.json' assert { type: 'json' };
+import { ethers } from 'ethers';
+
+import { getSigner } from "./aaveUtils";
+import {
+  PYTH_CONTRACT_ADDRESS_SEPOLIA,
+  PYTH_PRICE_FEED_ID_ETH_USD,
+  OUR_SMART_CONTRACT_ABI,
+  OUR_SMART_CONTRACT_ADDRESS,
+} from './constants';
+
+export async function executeOnChainPriceUpdate(): Promise<string> {
+  const signer = getSigner(11155111);
+  const connection = new HermesClient('https://hermes.pyth.network', {});
+  const priceUpdates = await connection.getLatestPriceUpdates([PYTH_PRICE_FEED_ID_ETH_USD]);
+
+  const pythContract = new ethers.Contract(PYTH_CONTRACT_ADDRESS_SEPOLIA, PythAbi, signer.provider);
+  const updateFee = await pythContract.getUpdateFee(priceUpdates);
+
+  const healthGuardianContract = new ethers.Contract(
+    OUR_SMART_CONTRACT_ADDRESS,
+    OUR_SMART_CONTRACT_ABI,
+    signer
+  );
+  const tx = await healthGuardianContract.updatePythPriceFeeds(priceUpdates, { value: updateFee });
+  await tx.wait();
+  const storedPrice = await healthGuardianContract.lastEthPrice();
+  const priceNumber = ethers.utils.formatUnits(storedPrice, 8);
+  return parseFloat(priceNumber).toString();
+}
