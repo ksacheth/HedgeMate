c4d50fc4ea67ff962f2ebd53e5f93ef50d9f09d0
DivyanshTech06 <divyanshtech06@gmail.com>
2025-10-21 21:29:16 +0530

    chore: edit index.ts and jobWorker.ts

--- packages/dca-backend/src/bin/jobWorker.ts
+++ packages/dca-backend/src/bin/jobWorker.ts
@@ -1,9 +1,11 @@
-import { startWorker } from '../lib/jobWorker';
+// import { startWorker } from '../lib/jobWorker';
+import { startAgenda } from '../lib/agenda/jobs/jobManagerInstance';
 import { serviceLogger } from '../lib/logger';
 
 async function gogo() {
   try {
-    await startWorker();
+    await startAgenda();
+    serviceLogger.info('✅ Job worker started and connected to Agenda.');
   } catch (error) {
     serviceLogger.error('!!! Failed to initialize service', error);
     throw error;
